101b109acec2b849287b0bca728eaefffbc11e16
JyunKim <kann1997@naver.com>
2020-11-18 01:52:03 +0900

    Add models

--- cssite/collect/views.py
+++ cssite/collect/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Account
 
-# Create your views here.
+def index(request):
+    account = Account.objects.get(pk=1)
+    context = {
+        'id': account.acc_id,
+        'name': account.name,
+    }
+    return render(request, 'collect/index.html', {'context': context})
